13c81461d11b44a11163f48255efadac56622b5b
John Allison <john@jallison.co.uk>
2024-07-25 09:16:48 +0100

    add bundle analysis

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   output: 'export'
 };
 
-export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
