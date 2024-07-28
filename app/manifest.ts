da3cf9acb08d3dfdf29f67fc3003598d18a96f28
John Allison <john@jallison.co.uk>
2024-07-28 17:44:26 +0100

    Create manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'ARMA Reforger Server Config Editor',
+    short_name: 'ARMA Reforger .json Editor',
+    description: 'JSON Editor for ARMA Reforger Server Config',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000',
+    theme_color: '#dc2626',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
