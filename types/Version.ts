769f5000b599e5cd310a93b61f0def65b7b4d05f
John Allison <john@jallison.co.uk>
2025-03-04 22:04:21 +0000

    Update Version.ts

--- types/Version.ts
+++ types/Version.ts
@@ -1,4 +1,4 @@
-export const UpdateVersion: string = "1.2.1.189"; //What the game is at currently, https://reforger.armaplatform.com/news
+export const UpdateVersion: string = "1.2.1.202"; //What the game is at currently, https://reforger.armaplatform.com/news
 export const CurrentVersion: string = "1.2.1.189"; //What the site is at currently
 
 export const isUpToDate = () => UpdateVersion === CurrentVersion;
